b3a2cce1c2ca4b40b25abd3c40f4ce2595d3037b
shbong <shbong06@gmail.com>
2026-09-17 18:52:03 +0900

    feat:오류4차

--- cobot3_ws/install/setup.bash
+++ cobot3_ws/install/setup.bash
@@ -21,12 +21,6 @@ _colcon_prefix_chain_bash_source_script() {
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/opt/ros/jazzy"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
-# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/rokey/IsaacSim-ros_workspaces/jazzy_ws/install"
-_colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
-# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/rokey/cobot3_ws/install"
-_colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
--- cobot3_ws/install/setup.ps1
+++ cobot3_ws/install/setup.ps1
@@ -24,8 +24,6 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/jazzy/local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/rokey/IsaacSim-ros_workspaces/jazzy_ws/install/local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/rokey/cobot3_ws/install/local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
